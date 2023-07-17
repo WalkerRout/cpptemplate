41b990eb7d2d7df977f1bc0326a310039d0a2b57
walkerrout04@gmail.com <walkerrout04@gmail.com>
2023-07-16 17:32:46 -0700

    Removed build files

--- build/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Workspace/cpp-workspace/cpptemplate/lib/googletest/googlemock
-# Build directory: C:/Workspace/cpp-workspace/cpptemplate/build/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/src/CMakeFiles/cpptemplate_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libcpptemplate_lib.a"
-)
